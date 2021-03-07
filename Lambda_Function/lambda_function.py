f6081e6339366b595d19bd82e9152fb209ccaed1
ryumasann <im20140319@gmail.com>
2021-03-08 03:03:54 +0900

    table.scanでGETでAPIから取得完了

--- Lambda_Function/lambda_function.py
+++ Lambda_Function/lambda_function.py
@@ -4,15 +4,17 @@ dynamodb = boto3.resource('dynamodb')
 #boto3.resource('')にサービス名を入れて操作するサービスを指定
 table = dynamodb.Table('MemberTable')
 
-def get_member(id):
-    response = table.get_item(#boto3の関数
-        Key={
-            'MemberId': id
-        }
-    )
-    return response['Item']#取得したItemは辞書型のためリストやタプルの表記
+#def get_member(id):
+#    response = table.get_item(#boto3の関数
+#        Key={
+#            'MemberId': id
+#        }
+#    )
+#    return response['Item']#取得したItemは辞書型のためリストやタプルの表記
 
-def lambda_handler(event, context):#ラムダ関数で呼び出される関数名と引数
     #「Runtime settings」のlambda_function.lambda_handler
-    member = get_member(event['MemberId'])#関数にid=001が入り呼び出される
-    return member
+def lambda_handler(event, context):#ラムダ関数で呼び出される関数名と引数
+    response = table.scan()
+    return response
+    #member = get_member(event['MemberId'])#関数にid=001が入り呼び出される
+    #return member
