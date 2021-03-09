6339505a701c0d32f743adbeb4dfbefa86423e58
ryumasann <im20140319@gmail.com>
2021-03-10 00:56:49 +0900

    POST実装

--- Lambda_Function/lambda_function.py
+++ Lambda_Function/lambda_function.py
@@ -14,18 +14,31 @@ table = dynamodb.Table('MemberTable')
 
     #「Runtime settings」のlambda_function.lambda_handler
 def lambda_handler(event, context):#ラムダ関数で呼び出される関数名と引数
-    tableData = table.scan(
-        #ReturnConsumedCapacity='TOTAL'を入れないとテーブル名を取得できない
-        ReturnConsumedCapacity='TOTAL'
-        )
-    try:
-        #scanでデータを取得できない場合は変数がからのためNameError
-        strTableName = tableData['ConsumedCapacity']['TableName']
-        dicTableItems = tableData['Items']
-        return strTableName, dicTableItems
-    
-    except NameError:
-        noRet = {"message":"There is no corresponding record"}
-        return noRet
-    #member = get_member(event['MemberId'])#関数にid=001が入り呼び出される
-    #return member
+    #eventはpathパラメタ、contextは
+    if len(event) == 0:
+    #GET処理(パラメーターを渡さない想定)
+        tableData = table.scan(
+            #ReturnConsumedCapacity='TOTAL'を入れないとテーブル名を取得できない
+            ReturnConsumedCapacity='TOTAL'
+            )
+        try:
+            #scanでデータを取得できない場合は変数がからのためNameError
+            strTableName = tableData['ConsumedCapacity']['TableName']
+            dicTableItems = tableData['Items']
+            return strTableName, dicTableItems
+        
+        except NameError:
+            noRet = {"message":"There is no corresponding record"}
+            return noRet
+        #member = get_member(event['MemberId'])#関数にid=001が入り呼び出される
+        #return member
+    else:
+    #POST処理
+        item = {
+            "MemberId": str(event["MemberId"]),
+            "Name": str(event["MemberName"])
+        }
+        
+        table.put_item(Item=item)
+        
+        return event
